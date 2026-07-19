8e91c4ba5d32e3fbd87001f30fe9d961827400cb
Vane4ka2k2 <41358676+Vane4ka2k2@users.noreply.github.com>
2026-07-19 23:50:21 +0300

    Tidy up project: clean build artifacts, add gitignore, add README

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vm180/projects/NewMatrix
-# Build directory: /home/vm180/projects/NewMatrix/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MatrixTests]=] "/home/vm180/projects/NewMatrix/build/matrix_tests")
-set_tests_properties([=[MatrixTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/vm180/projects/NewMatrix/CMakeLists.txt;35;add_test;/home/vm180/projects/NewMatrix/CMakeLists.txt;0;")
